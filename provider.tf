bf3b7656aa8140ae6f13a92aa739d8b2d18392bb
AtharvaWathodkar <111409657+AtharvaWathodkar@users.noreply.github.com>
2024-01-25 13:13:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,16 @@
+locals {
+  gcp_service_account = jsondecode(var.GOOGLE_APPLICATION_CREDENTIALS)
+}
+
 provider "google" {
   project = "cool-furnace-410107"
   region  = "us-central1"
+  credentials =  jsonencode(local.gcp_service_account)
 }
 
 provider "google" {
   alias   = "project_02"
   project = "poetic-hawk-354105"
   region  = "us-central1"
+  credentials =  jsonencode(local.gcp_service_account)
 }
