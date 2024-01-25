8517b92730e8bb52ac7fc2a97f0e3302c93e7eb8
AtharvaWathodkar <111409657+AtharvaWathodkar@users.noreply.github.com>
2024-01-25 14:17:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,41 @@
-resource "google_bigquery_dataset" "project_01_dataset" {
-  dataset_id = "project_01_dataset"
-  default_table_expiration_ms = 60 * 24 * 60 * 60 * 1000 # 60 days in milliseconds
-}
-
-resource "google_bigquery_dataset" "project_02_dataset" {
-  provider = google.project_02
-  dataset_id = "project_02_dataset"
-  default_table_expiration_ms = 60 * 24 * 60 * 60 * 1000 # 60 days in milliseconds
-}
-
-resource "google_bigquery_table" "project_02_table" {
-  provider   = google.project_02
-  dataset_id = google_bigquery_dataset.project_02_dataset.dataset_id
-  table_id   = "project_02_table"
-
-  schema = <<EOF
-[
-  {
-    "name": "column1",
-    "type": "STRING",
-    "mode": "NULLABLE"
-  },
-  {
-    "name": "column2",
-    "type": "INTEGER",
-    "mode": "NULLABLE"
-  }
-]
-EOF
-}
-
-resource "google_bigquery_table" "project_01_view" {
-  dataset_id = google_bigquery_dataset.project_01_dataset.dataset_id
-  table_id   = "project_01_view"
-  view {
-    query          = "SELECT * FROM `${google_bigquery_table.project_02_table.project}.${google_bigquery_table.project_02_table.dataset_id}.${google_bigquery_table.project_02_table.table_id}`"
-    use_legacy_sql = false
-  }
-}
+// resource "google_bigquery_dataset" "project_01_dataset" {
+//     dataset_id = "project_01_dataset"
+//     default_table_expiration_ms = 60 * 24 * 60 * 60 * 1000 # 60 days in milliseconds
+//   }
+  
+//   resource "google_bigquery_dataset" "project_02_dataset" {
+//     provider = google.project_02
+//     dataset_id = "project_02_dataset"
+//     default_table_expiration_ms = 60 * 24 * 60 * 60 * 1000 # 60 days in milliseconds
+//   }
+  
+//   resource "google_bigquery_table" "project_02_table" {
+//     provider   = google.project_02
+//     dataset_id = google_bigquery_dataset.project_02_dataset.dataset_id
+//     table_id   = "project_02_table"
+  
+//     schema = <<EOF
+//   [
+//     {
+//       "name": "column1",
+//       "type": "STRING",
+//       "mode": "NULLABLE"
+//     },
+//     {
+//       "name": "column2",
+//       "type": "INTEGER",
+//       "mode": "NULLABLE"
+//     }
+//   ]
+//   EOF
+//   }
+  
+//   resource "google_bigquery_table" "project_01_view" {
+//     dataset_id = google_bigquery_dataset.project_01_dataset.dataset_id
+//     table_id   = "project_01_view"
+//     view {
+//       query          = "SELECT * FROM `${google_bigquery_table.project_02_table.project}.${google_bigquery_table.project_02_table.dataset_id}.${google_bigquery_table.project_02_table.table_id}`"
+//       use_legacy_sql = false
+//     }
+//   }
+  
