00abe6f60776b651094be2596464f29337254531
AtharvaWathodkar <111409657+AtharvaWathodkar@users.noreply.github.com>
2024-01-25 12:37:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,32 @@ resource "google_bigquery_dataset" "project_01_dataset" {
   dataset_id = "project_01_dataset"
 }
 
+resource "google_bigquery_dataset" "project_02_dataset" {
+  provider = google.project_02
+  dataset_id = "project_02_dataset"
+}
+
+resource "google_bigquery_table" "project_02_table" {
+  provider   = google.project_02
+  dataset_id = google_bigquery_dataset.project_02_dataset.dataset_id
+  table_id   = "project_02_table"
+
+  schema = <<EOF
+[
+  {
+    "name": "column1",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "column2",
+    "type": "INTEGER",
+    "mode": "NULLABLE"
+  }
+]
+EOF
+}
+
 resource "google_bigquery_table" "project_01_view" {
   dataset_id = google_bigquery_dataset.project_01_dataset.dataset_id
   table_id   = "project_01_view"
