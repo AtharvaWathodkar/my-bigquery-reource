7a22ef1d7a9c3fd2f277187d467a12373520cbd3
AtharvaWathodkar <111409657+AtharvaWathodkar@users.noreply.github.com>
2024-01-25 13:36:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 provider "google" {
   project = "cool-furnace-410107"
   region  = "us-central1"
+  credentials = "cool-furnace-410107-3cafbcf95099.json"
 }
 
 provider "google" {
   alias   = "project_02"
   project = "poetic-hawk-354105"
   region  = "us-central1"
-
+  credentials = "cool-furnace-410107-3cafbcf95099.json"
 }
